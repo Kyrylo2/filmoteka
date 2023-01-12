79ac1e4e328d6f42657ebf8872bbd4b98b8e49aa
Kyrylo <savchenko.kyrylo@gmail.com>
2023-01-12 21:06:08 +0200

    Update refs.js

--- src/js/utils/refs.js
+++ src/js/utils/refs.js
@@ -1,5 +1,6 @@
 const refs = {
   search: document.querySelector('#searchForm'),
+  filmsMainContainer: document.querySelector('.films__list'),
 };
 
-export const { search } = refs;
+export const { search, filmsMainContainer } = refs;
