486c2efa4deb5c204d67da4fcf95f665b1b63c4d
Mozzhakov <imozzhakov@icloud.com>
2023-01-13 20:28:51 +0200

    add fetch for full info and render it

--- src/js/utils/refs.js
+++ src/js/utils/refs.js
@@ -1,6 +1,7 @@
 const refs = {
   search: document.querySelector('#searchForm'),
   filmsMainContainer: document.querySelector('.films__list'),
+  modal: document.querySelector('.modal'),
 };
 
-export const { search, filmsMainContainer } = refs;
+export const { search, filmsMainContainer, modal } = refs;
