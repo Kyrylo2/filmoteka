d2859d682ba1dc0811c5f02d020c4198683bb3c0
Olga Panayot <panayot.olga@gmail.com>
2023-01-14 12:48:32 +0200

    html, js, styles for desktop

---
+++ src/js/students.js
@@ -0,0 +1,34 @@
+
+
+const refs = {
+openModalBtn: document.querySelector(".footer__button"),
+closeModalBtn: document.querySelector("[data-modal-close]"),
+backdrop: document.querySelector('.backdrop')
+}
+
+refs.openModalBtn.addEventListener('click', onOpenModal);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.backdrop.addEventListener('click', onBackdropClick);
+
+
+function onOpenModal() {
+    window.addEventListener('keydown', onEscKeydown);
+    document.body.classList.add('show-modal');	
+}
+
+function onCloseModal() {
+    window.removeEventListener('keydown', onEscKeydown);
+    document.body.classList.remove('show-modal');
+}
+
+function onBackdropClick(e) {
+    if (e.currentTarget === e.target) {
+        onCloseModal();
+    }
+}
+
+function onEscKeydown(e) {
+    if (e.code === 'Escape') {
+      onCloseModal();  
+    }    
+}
