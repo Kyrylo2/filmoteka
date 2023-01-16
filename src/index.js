4f0e8ba831c3b22465bec20290d925d467ac11fb
Kyrylo <savchenko.kyrylo@gmail.com>
2023-01-16 22:14:38 +0200

    sort pagination + fix

--- src/index.js
+++ src/index.js
@@ -168,7 +168,7 @@ function addSortingYears() {
   const minYear = 1999;
   const maxYear = 2022;
   const arrOfYears = [];
-  for (let i = minYear; i <= maxYear; i++) {
+  for (let i = maxYear; i - 1 > minYear; i--) {
     arrOfYears.push(`<option value="${i}">${i}</option>`);
   }
   // console.log(arrOfYears);
@@ -194,4 +194,19 @@ async function onSortFormSubmit(e) {
   // clearMarkup();
   const arrOfMovies = await moviesApiService.getSortedMovies();
   createMarkup(renderMovies(arrOfMovies));
+
+  const pagination = new Pagination(
+    'tui-pagination-container',
+    moviesApiService.PaginationOptions
+  );
+
+  pagination.on('beforeMove', async e => {
+    try {
+      moviesApiService.page = e.page;
+      const arrOfMovies = await moviesApiService.getSortedMovies();
+      createMarkup(renderMovies(arrOfMovies));
+    } catch (e) {
+      console.log(e);
+    }
+  });
 }
