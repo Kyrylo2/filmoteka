8ce5e56af5a254ee5f8a69a2987b8a54aa1b92a2
Katya-Semeniuk <katyasemeniuk@gmail.com>
2023-01-16 17:11:50 +0100

    image

--- src/js/utils/movie-api.js
+++ src/js/utils/movie-api.js
@@ -35,12 +35,16 @@ class MoviesApiServise {
         currentPage:
           '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
         moveButton:
-         '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+          '<a href="#" class="tui-page-btn tui-{{type}}">' +
+          '<svg fill="#000000" width="40px" height="40px" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg">' +
+          '<path d="M214.78,478l-20.67-21.57L403.27,256,194.11,55.57,214.78,34,446.46,256ZM317.89,256,86.22,34,65.54,55.57,274.7,256,65.54,456.43,86.22,478Z" />' +
+          '</svg> '+
+                //  '<span class="tui-ico-{{type}}">{{type}}</span>' +
              '</a>',
          disabledMoveButton:
            '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+           '<span class="tui-ico-{{type}}">{{type}}</span>' +
+          '<?xml version="1.0" ?>' +
              '</span>',
         moreButton:
           '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
@@ -49,6 +53,9 @@ class MoviesApiServise {
       },
     };
   }
+
+// <?xml version="1.0" ?><svg fill="#000000" width="800px" height="800px" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><path d="M214.78,478l-20.67-21.57L403.27,256,194.11,55.57,214.78,34,446.46,256ZM317.89,256,86.22,34,65.54,55.57,274.7,256,65.54,456.43,86.22,478Z"/></svg>
+
 
   async fetchMovies() {
     Loading.circle({ svgColor: 'red' });
